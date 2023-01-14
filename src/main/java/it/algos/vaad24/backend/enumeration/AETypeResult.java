5fd641823ba57e77c387861bff21010afe9a2ec9
algos-soft <info@algos.it>
2023-01-14 18:40:27 +0100

    ...

---
+++ src/main/java/it/algos/vaad24/backend/enumeration/AETypeResult.java
@@ -0,0 +1,51 @@
+package it.algos.vaad24.backend.enumeration;
+
+import it.algos.vaad24.backend.interfaces.*;
+
+import java.util.*;
+
+/**
+ * Project vaad24
+ * Created by Algos
+ * User: gac
+ * Date: Sat, 14-Jan-2023
+ * Time: 15:46
+ */
+public enum AETypeResult implements AIType {
+    indeterminato("Indeterminato"),
+    noAECopy("Manca il type AECopy"),
+    typeNonCompatibile("Il type previsto non è compatibile col metodo"),
+    noFileName("Manca il nome del file"),
+
+    fileEsistente("Il file esisteva già"),
+    fileCreato("Il file è stato creato"),
+    fileSovrascritto("Il file è stato sovrascritto");
+
+    private String tag;
+
+    AETypeResult(String tag) {
+        this.tag = tag;
+    }
+
+    public static List<AETypeResult> getAllEnums() {
+        return Arrays.stream(values()).toList();
+    }
+
+    public static List<String> getAllTags() {
+        List<String> listaTags = new ArrayList<>();
+
+        getAllEnums().forEach(type -> listaTags.add(type.getTag()));
+        return listaTags;
+    }
+
+    @Override
+    public List<AETypeResult> getAll() {
+        return Arrays.stream(values()).toList();
+    }
+
+    @Override
+    public String getTag() {
+        return tag;
+    }
+
+}
