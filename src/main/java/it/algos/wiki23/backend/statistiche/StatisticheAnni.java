d557d0715427031d2f2eed58b1a24e88c3251e73
algos-soft <info@algos.it>
2023-01-24 08:35:31 +0100

    8.2

--- src/main/java/it/algos/wiki23/backend/statistiche/StatisticheAnni.java
+++ src/main/java/it/algos/wiki23/backend/statistiche/StatisticheAnni.java
@@ -66,7 +66,7 @@ public class StatisticheAnni extends Statistiche {
      * Elabora i dati
      */
     protected void elabora() {
-        //        annoWikiBackend.elabora();
+                annoWikiBackend.elabora();
         Map mappa = annoWikiBackend.elaboraValidi();
     }
 
